db7c764159beaf27be8f0b001242706b2cec2525
Marc Alban <malban@swri.org>
2013-04-05 09:21:40 -0500

    add correct copyright headers

--- mapviz_plugins/src/multires_image_plugin.cpp
+++ mapviz_plugins/src/multires_image_plugin.cpp
@@ -1,3 +1,22 @@
+// *****************************************************************************
+//
+// Copyright (C) 2013 All Right Reserved, Southwest Research Institute® (SwRI®)
+//
+// Contract No.  10-58058A
+// Contractor    Southwest Research Institute® (SwRI®)
+// Address       6220 Culebra Road, San Antonio, Texas 78228-0510
+// Contact       Steve Dellenback <sdellenback@swri.org> (210) 522-3914
+//
+// This code was developed as part of an internal research project fully funded
+// by Southwest Research Institute®.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// *****************************************************************************
+
 // C++ standard libraries
 #include <cstdio>
 
--- multires_image/include/multires_image/point.h
+++ multires_image/include/multires_image/point.h
@@ -1,11 +1,22 @@
-/*
- * Point.h
- * 
- * Copyright (C) 2010 Southwest Research Institute <tbrown@swri.org>
- *
- * Created on: Feb 22, 2010
- *     Author: Tucker Brown
- */
+// *****************************************************************************
+//
+// Copyright (C) 2013 All Right Reserved, Southwest Research Institute® (SwRI®)
+//
+// Contract No.  10-58058A
+// Contractor    Southwest Research Institute® (SwRI®)
+// Address       6220 Culebra Road, San Antonio, Texas 78228-0510
+// Contact       Steve Dellenback <sdellenback@swri.org> (210) 522-3914
+//
+// This code was developed as part of an internal research project fully funded
+// by Southwest Research Institute®.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// *****************************************************************************
+
 #ifndef MULTIRES_IMAGE_POINT_H_
 #define MULTIRES_IMAGE_POINT_H_
 
