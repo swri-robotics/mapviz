14b92925e0e40fbabef7229e004a3a273b55409d
Edward Venator <evenator@swri.org>
2015-07-04 13:17:51 -0500

    Fixes copyright dates.

--- mapviz_plugins/include/mapviz_plugins/laserscan_plugin.h
+++ mapviz_plugins/include/mapviz_plugins/laserscan_plugin.h
@@ -1,6 +1,6 @@
 // *****************************************************************************
 //
-// Copyright (c) 2014, Southwest Research Institute® (SwRI®)
+// Copyright (c) 2015, Southwest Research Institute® (SwRI®)
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
