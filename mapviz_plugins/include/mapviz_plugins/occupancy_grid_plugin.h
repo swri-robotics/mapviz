43347dd892b4bf74ca34d1033c30cfebac0986a0
Davide Faconti <davide.faconti@gmail.com>
2018-01-31 10:34:31 +0100

    copyright fix

--- mapviz_plugins/include/mapviz_plugins/occupancy_grid_plugin.h
+++ mapviz_plugins/include/mapviz_plugins/occupancy_grid_plugin.h
@@ -1,6 +1,6 @@
 // *****************************************************************************
 //
-// Copyright (c) 2014, Southwest Research Institute® (SwRI®)
+// Copyright (c) 2018, Eurecat
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
 //     * Redistributions in binary form must reproduce the above copyright
 //       notice, this list of conditions and the following disclaimer in the
 //       documentation and/or other materials provided with the distribution.
-//     * Neither the name of Southwest Research Institute® (SwRI®) nor the
+//     * Neither the name of Eurecat nor the
 //       names of its contributors may be used to endorse or promote products
 //       derived from this software without specific prior written permission.
 //
