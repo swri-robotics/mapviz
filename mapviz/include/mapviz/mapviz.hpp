fba34e5aa94e53be7b9462167fa3b39011ad13e5
Jacob Hassold <jhassold@dcscorp.com>
2020-02-19 11:50:10 -0500

    Properly create shared service pointer Distro A, OPSEC #2893
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- mapviz/include/mapviz/mapviz.hpp
+++ mapviz/include/mapviz/mapviz.hpp
@@ -180,7 +180,7 @@ protected:
 
   std::shared_ptr<rclcpp::Node> node_;
   // rclcpp::ServiceServer add_display_srv_;
-  rclcpp::Service<mapviz::srv::AddMapvizDisplay> add_display_srv_;
+  rclcpp::Service<mapviz::srv::AddMapvizDisplay>::SharedPtr add_display_srv_;
   std::shared_ptr<tf2_ros::Buffer> tf_buf_;
   std::shared_ptr<tf2_ros::TransformListener> tf_;
   swri_transform_util::TransformManagerPtr tf_manager_;
