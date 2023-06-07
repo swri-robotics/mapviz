20e69842af99174901753d31ece906d5043651ab
David Anthony <david.anthony@swri.org>
2023-06-07 13:55:27 -0500

    Adding conditional compilation to switch between .h and .hpp files for different ROS distros

--- mapviz/include/mapviz/map_canvas.h
+++ mapviz/include/mapviz/map_canvas.h
@@ -41,7 +41,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/transform_datatypes.h>
 #include <tf2_ros/transform_listener.h>
+#ifdef USE_TF2_H_FILES
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#else
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#endif
 
 #include <mapviz/mapviz_plugin.h>
 
